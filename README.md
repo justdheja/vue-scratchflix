01f90af8ea999103862af704f7bb3de4e5719577
justdheja <justdheja@gmail.com>
2020-12-04 07:42:21 +0700

    add navbar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# netflix
+# Scratchflix
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <the-navbar></the-navbar>
     <router-view/>
   </div>
 </template>
 
+<script>
+import TheNavbar from './components/TheNavbar'
+export default {
+  components: {
+    TheNavbar
+  }
+}
+</script>
+
 <style lang="scss">
+body {
+  min-height: 100vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: #2c3e50;
 }
 
 .nflogo {
